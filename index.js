9c7329f440b2521f2f7a8ba2786cb69b2c24d5d8
tienthinguyen <tienthinguyen@kms-technology.com>
2024-02-03 15:06:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,98 @@
-const express = require("express");
-const app = express();
-const http = require("http").Server(app);
-const cors = require("cors");
-const PORT = 4000;
+const express = require("express")
+const app = express()
+const http = require("http").Server(app)
+const cors = require("cors")
+const PORT = 4000
 const socketIO = require("socket.io")(http, {
-	cors: {
-		origin: "http://localhost:3000",
-	},
-});
+  cors: {
+    origin: "http://localhost:3000",
+  },
+})
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
 
-const generateID = () => Math.random().toString(36).substring(2, 10);
-let chatRooms = [];
+const generateID = () => Math.random().toString(36).substring(2, 10)
+let chatRooms = []
+
+const generateRoomId = (userA, userB) => {
+  if (userA > userB) {
+    return userB + userA;
+  }
+  else
+    return userA + userB;
+}
 
 socketIO.on("connection", (socket) => {
-	console.log(`⚡: ${socket.id} user just connected!`);
+  console.log(`⚡: ${socket.id} user just connected!`)
 
-	socket.on("createRoom", (room) => {
-		console.log("create Room , name = ", room.name);
-		socket.join(room.name);
-		chatRooms.unshift({ id: room.id, name: room.name, messages: [] });
-		socket.emit("roomsList", chatRooms);
-		console.log("roomsList = ", chatRooms);
-	});
+  socket.on("createRoom", (data) => {
+    const roomId = generateRoomId(data.userA, data.userB)
+    console.log("create Room , name = ", roomId)
+    socket.join(roomId)
+    chatRooms.unshift({ id: roomId, name: roomId, userA: userA, userB: userB, messages: [] })
+    socket.emit("roomsList", chatRooms)
+    console.log("roomsList = ", chatRooms)
+  })
 
-	socket.on("findRoom", (id) => {
-		let result = chatRooms.filter((room) => room.id == id);
-		console.log("result room", result);
-		if (result && result.length > 0) {
-			socket.emit("foundRoom", result[0].messages);
-			console.log("Messages Form", result[0].messages);
-		}
-		else {
-			socket.emit("foundRoom", result);
-		}
-	});
+  socket.on("findRoom", (data) => {
+    const roomId = generateRoomId(data.userA, data.userB)
+    let result = chatRooms.filter((room) => room.id == roomId)
+    console.log("result findRoom", result)
+    if (result && result.length > 0) {
+      socket.emit("foundRoom", result[0].messages)
+    }
+  })
 
-	socket.on("newMessage", (data) => {
-		console.log("data newmessage", data)
-		console.log("chatRooms", chatRooms)
-		const { room_id, message, user, timestamp } = data;
-		let result = chatRooms.filter((room) => room.id == room_id);
-		console.log("result room", result);
-		const newMessage = {
-			id: generateID(),
-			text: message,
-			user,
-			time: `${timestamp.hour}:${timestamp.mins}`,
-		};
-		if (result && result.length > 0) {
-			socketIO.to(result[0].name).emit("roomMessage", newMessage);
-			result[0].messages.push(newMessage);
+  socket.on("findRoomList", (data) => {
+    console.log("findRoomList", data.userA)
+    let result = chatRooms.filter((room) => room.id.includes(data.userA))
+    console.log("result findRoomList", result)
+    if (result && result.length > 0) {
+      socket.emit("foundRoom", result[0].messages)
+    }
+  })
 
-			socketIO.emit("roomsList", chatRooms);
-			console.log("Emit roomsList  ", chatRooms);
-			socketIO.emit("foundRoom", result[0].messages);
-			console.log("Emit foundRoom  ", result[0].messages);
-		}
-	});
-	socket.on("disconnect", () => {
-		socket.disconnect();
-		console.log("🔥: A user disconnected");
-	});
-});
+  socket.on("newMessage", (data) => {
+    console.log("data newmessage", data)
+    const { message, userA, userB, timestamp } = data
+    const room_id = generateRoomId(userA, userB)
+    let result = chatRooms.filter((room) => room.id == room_id)
+    console.log("room result in newMessage:", result)
+    if (result.length == 0) {
+      console.log("create Room , name = ", room_id)
+      socket.join(room_id)
+      chatRooms.unshift({ id: room_id, name: room_id, userA: userA, userB: userB, messages: [] })
+      socket.emit("roomsList", chatRooms)
+      console.log("roomsList = ", chatRooms)
+    }
+    result = chatRooms.filter((room) => room.id == room_id)
+    console.log("room result in newMessage22222:", result)
+    const newMessage = {
+      id: generateID(),
+      text: message,
+      userA,
+      time: `${timestamp.hour}:${timestamp.mins}`,
+    }
+    socketIO.to(result[0].name).emit("roomMessage", newMessage)
+    result[0].messages.push(newMessage)
+
+    socketIO.emit("roomsList", chatRooms)
+    console.log("Emit roomsList  ", chatRooms)
+    socketIO.emit("foundRoom", result[0].messages)
+    console.log("Emit foundRoom  ", result[0].messages)
+  })
+  socket.on("disconnect", () => {
+    socket.disconnect()
+    console.log("🔥: A user disconnected")
+  })
+})
 
 app.get("/api", (req, res) => {
-	res.json(chatRooms);
-});
+  res.json(chatRooms)
+})
 
 http.listen(PORT, () => {
-	console.log(`Server listening on ${PORT}`);
-});
+  console.log(`Server listening on ${PORT}`)
+})
